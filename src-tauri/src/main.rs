dbecfc48ac06bb9800a880daaca1c1bbd462c0a4
张鑫 <zhangxin@zhangxindeMacBook-Pro.local>
2023-10-05 18:24:14 +0800

    fix: fix proxy config file path

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::env;
+use std::path::Path;
 use std::sync::Arc;
 use std::sync::Mutex;
 
@@ -8,23 +10,32 @@ use pipeJoint::ProxyManager;
 
 #[tokio::main]
 async fn main() {
-    // init manager
-    let mut proxy_manager = ProxyManager::new("../proxy_list.json");
+    let config_file = Path::new(&env::current_dir().unwrap()).join("proxy_list.json");
+    let binding = config_file.into_os_string();
+    let cwd = binding.to_str();
+    if let Some(save_path) = cwd {
+        println!("config save dir: {}", save_path);
 
-    // init proxy sedrver
-    init_proxy_server(&mut proxy_manager).await;
+        // init manager
+        let mut proxy_manager = ProxyManager::new(save_path);
 
-    // init tauri
-    tauri::Builder::default()
-        .manage(Arc::new(Mutex::new(proxy_manager)))
-        .invoke_handler(tauri::generate_handler![
-            cmds::get_proxy_list,
-            cmds::buck_proxy,
-            cmds::del_proxy_item,
-            cmds::set_proxy_status
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        // init proxy sedrver
+        init_proxy_server(&mut proxy_manager).await;
+
+        // init tauri
+        tauri::Builder::default()
+            .manage(Arc::new(Mutex::new(proxy_manager)))
+            .invoke_handler(tauri::generate_handler![
+                cmds::get_proxy_list,
+                cmds::buck_proxy,
+                cmds::del_proxy_item,
+                cmds::set_proxy_status
+            ])
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
+    } else {
+        println!("save path is None");
+    }
 }
 
 /**
